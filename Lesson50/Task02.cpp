6e87c1c0994e8e70bf728b75e0a7b0bb83db6b32
Meam1c <matvey.jilkevich@gmail.com>
2023-03-16 18:47:35 +0300

    change project

--- Lesson50/Task02.cpp
+++ Lesson50/Task02.cpp
@@ -1,18 +1,16 @@
 #include<iostream>
+#include"logic.h"
+
 using namespace std;
 
-int sum(int a, int b);
-int sub(int a, int b);
-
-
 int main() {
-	int a, b;
+	int a, b,c;
 
-	cout << "inpute a and b:" << endl;
-	cin >> a >> b;
+	cout << "inpute a and b and c:" << endl;
+	cin >> a >> b>>c;
 
-	cout << "a - b =" << sub(a, b) << endl;
-	cout << "a + b =" << sum(a, b) << endl;
+	cout << "a - b - c =" << sub(a, b, c) << endl;
+	cout << "a + b + c =" << sum(a, b,c) << endl;
 
 	return 0;
 }
